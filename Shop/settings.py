2066683c950bd5bb9d99e6430aa6f2e38b6997f8
Your Name <you@example.com>
2023-03-12 23:37:56 +0200

    for herroku

--- Shop/settings.py
+++ Shop/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = ['italian-restaurantt.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
-    'account'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig'
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
