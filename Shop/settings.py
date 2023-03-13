6d850475f39daaaaf9c1b8baed6250f8ebd79798
Your Name <you@example.com>
2023-03-13 14:38:32 +0200

    for herroku

--- Shop/settings.py
+++ Shop/settings.py
@@ -1,5 +1,8 @@
 import os
 from pathlib import Path
+import django_heroku
+
+django_heroku.settings(locals(), staticfiles=False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
