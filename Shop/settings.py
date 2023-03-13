811747d402c6622e6da0c2405c82177ba7bd4405
Your Name <you@example.com>
2023-03-13 15:53:40 +0200

    dd images

--- Shop/settings.py
+++ Shop/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
